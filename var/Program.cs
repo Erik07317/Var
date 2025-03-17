669bb429ec1a4fa5dc107803304a4393fb920628
Erik07317 <eriksabino2030@gmail.com>
2025-03-17 09:04:35 -0300

    Add files via upload
    aula 5 fatec

---
+++ var/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata;
+using System.Threading.Tasks.Dataflow;
+Console.Clear();
+string nome = "Erik";
+string sobrenome = "Sabino";
+
+Console.Write($"{nome.PadRight(11, '.')}: ");
+nome = Console.ReadLine();
+Console.Write($"{sobrenome.PadRight(11, '.')}: ");
+sobrenome = Console.ReadLine();
+Console.WriteLine($"Meu nome é {nome} {sobrenome}");
+
+// Console.Write("meu nome é ");
+// Console.Write(nome);
+// Console.Write(" ");
+// Console.WriteLine(sobrenome);
+
+
+
